1294135291628bc15dfae699b94337c4b55b7dde
Marcel Kurovski <mkurovski@Marcels-MBP.fritz.box>
2023-12-10 23:17:32 +0100

    feat: add solution 1-1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+//use std::env;
+use std::fs;
+
 fn main() {
     println!("Hello, world!");
+    let day1_filepath: &str = "data/day1_input.txt";
+    let contents = fs::read_to_string(day1_filepath).expect("Error");
+
+    let mut sum: u32 = 0;
+    let mut number: u32 = 0;
+
+    //iterate and sum over
+    for line in contents.lines() {
+        let mut start_num:char = '0';
+        let mut end_num:char = '0';
+        let reversed: String = line.chars().rev().collect();
+        for c in line.chars() {
+            if c.is_digit(10) {
+                start_num = c;
+                break;
+            }
+        }
+        for c in reversed.chars() {
+            if c.is_digit(10) {
+                end_num = c;
+                break;
+            }
+        }
+        number = format!("{}{}", start_num, end_num).parse().unwrap();
+        sum += number;
+    }
+    println!("Sum is {sum}");
 }
+
